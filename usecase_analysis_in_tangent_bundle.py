ca7477bd41cfc563560cd11d4fe54c8cbf314bfd
Esfandiar Navayazdani <102630546+enavayaz@users.noreply.github.com>
2022-05-10 22:44:41 +0200

    Update usecase_analysis_in_tangent_bundle.py
    visKenTPCA->visTPCA

--- usecase_analysis_in_tangent_bundle.py
+++ usecase_analysis_in_tangent_bundle.py
@@ -1,6 +1,8 @@
 # ln 266 in pre_shape.py -> add full_matrices=False to svd call (otherwise no autodiff)
-#from geomstats.geometry.pre_shape import PreShapeSpace, KendallShapeMetric
-from pre_shape_autodiff import PreShapeSpace, KendallShapeMetric
+# from geomstats.geometry.pre_shape import PreShapeSpace, KendallShapeMetric
+import os
+
+os.environ['GEOMSTATS_BACKEND'] = 'autograd'
 
 from sasaki_metric import SasakiMetric
 from geomstats.geometry.hypersphere import Hypersphere
@@ -8,109 +10,91 @@ from geomstats.learning.frechet_mean import FrechetMean
 from geomstats.learning.pca import TangentPCA
 from geomstats.learning.geodesic_regression import GeodesicRegression
 import geomstats.backend as gs
-from util import visSphere, visKen, visKenPCA, load_data, initial_mean
+import numpy as np
+from util import load_data_hur, visSphere, visEarth, initial_mean, visTPCA, coord_2D3D, preprocess_hur, get_CatName
 
-Nt = 25
-t = gs.linspace(0, 1, Nt)
-"""
-First Application: Discrete Geodesics on the 2-Sphere
-"""
 
+"""
+Application: Hurricane Tracks
+"""
+# Preprocessing, prepare data
+#preprocess_hur()
+
+# Import data
+data = np.load('hur.npz', allow_pickle=True)
+subj, seq, ids, info = data['subj'], data['seq'], data['ids'], data['info']
+n_subj = 21  # only 2010
+n_samples = ids[n_subj]
+# subj, seq, ids = subj[0:n_subj], seq[0:n_samples], ids[0:n_subj+1]
+# dataset contains 21 Hurricane tracks
+print(f"Number of subjects: {n_subj}, Total number of observations: {n_samples}")
+
+a=get_CatName(95)
+
+# Vis 5 subjects
+seq_list = []
+for i in range(192, 193):
+    seq_list.append(seq[ids[i]:ids[i + 1], 3:6])
+visEarth(seq_list)
+
+# geo_list = []
+# for i in range(18, 21):
+#     geo_list.append(seq[ids[i]:ids[i + 1], 7:10])
+# visSphere(geo_list, ['r'] + ['b'] + ['r'] + ['b'] + ['g'])
+
+# Prepare Riemannian structure
 S2 = Hypersphere(dim=2)
 S2_metric = S2.metric
-# bypass geomstats bug: shape property of HypersphereMetric not initialized consistently
 if S2.default_coords_type == 'extrinsic':
-    S2_metric.shape = (S2.dim+1,)
+    S2_metric.shape = (S2.dim + 1,)
 sas = SasakiMetric(S2.metric)
-p0, u0 = gs.array([0., -1, 0]), gs.array([1., 0, 1])
-pu0 = gs.array([p0, u0])
-pL, uL = gs.array([1., 0, 0]), gs.array([0., 1, 1])
-puL = gs.array([pL, uL])
-print('Computing shortest path of geodesics')
-z = sas.geodesic_discrete(pu0, puL)
-geo_list, color_list = [], []
-geods0L, geods = [], []
-geods0L.append(S2_metric.geodesic(p0, initial_tangent_vec=u0)(t))
-geods0L.append(S2_metric.geodesic(pL, initial_tangent_vec=uL)(t))
-geo_list = [geods0L]
-for j in range(1, len(z) - 1):
-    p1, u1 = z[j][0], z[j][1]
-    geods.append(S2_metric.geodesic(p1, initial_tangent_vec=u1)(t))
-geo_list += [geods]
-visSphere(geo_list, ['r']+['b'], 15)
 
 """
-Generate random sample points in tangent bundle with mean m
+Geodesics (via regression) and Mean Geodesic
 """
-m = gs.array([[0, -1.0, 0], [0, 0, 1.0]])
-
-n_samples, sigma = 10, gs.pi/12
-x = S2.random_riemannian_normal(m[0], n_samples=n_samples)
-y = S2.random_riemannian_normal(m[0], n_samples=n_samples)
-x = [S2_metric.exp(sigma*S2_metric.log(x[i], m[0]), m[0]) for i in range(n_samples)]
-u = [m[1] + sigma*S2_metric.log(y[i], m[0]) for i in range(n_samples)]
-samples = gs.array([[x[i], u[i]] for i in range(n_samples)])
-
-print('Computing mean of geodesics')
-initial = initial_mean(samples, S2_metric)
-mean_gs = FrechetMean(sas, init_point=initial)
-mean_gs.fit(samples)
-mean = mean_gs.estimate_
-# mean = sas.mean(samples)
-
-data, meanvalue, geom = [], [], []
-meanvalue.append(S2_metric.geodesic(m[0], initial_tangent_vec=m[1])(t))
-geom.append(S2_metric.geodesic(mean[0], initial_tangent_vec=mean[1])(t))
-for sample in samples:
-    data.append(S2_metric.geodesic(sample[0], initial_tangent_vec=sample[1])(t))
-visSphere([data] + [meanvalue] + [geom], ['r'] + ['k'] + ['b'], 15)
-
-"""
-Third application: Discrete Geodesics and Mean Geodesic in Kendall's Shape Space
-"""
-samples = load_data()
-n_samples = len(samples)
-n_subjects, n_trj, k_landmarks, dim = 18, 8, 8, 2
-print(f"Rat skulls data: {k_landmarks} planar landmarks from {n_trj} observations of {n_subjects} subjects")
-Ken = PreShapeSpace(k_landmarks, dim)
-KenMetric = KendallShapeMetric(k_landmarks, dim)
-KenMetric.shape = Ken.shape # fix: shape not set consistently
-sas = SasakiMetric(KenMetric)
-samples = [Ken.projection(samples[i]) for i in range(n_samples)]
-visKen([samples], ['r'])
-samples = gs.reshape(gs.array(samples), (n_subjects, n_trj, k_landmarks, dim))
 # Regression
-reg = GeodesicRegression(Ken, KenMetric, center_X=False, method="riemannian", initialization="warm_start")
-# TODO
-geos, geodesics = [], []
-x = gs.linspace(0., 1., n_trj)
-for trj in samples:
+reg = GeodesicRegression(S2, S2_metric, center_X=False, method="riemannian", initialization="frechet")
+# assume irregular time sampling to compensate for saturation in growth -> equidistant spread in shape space
+points, cats = seq[:, 7:10], seq[:, 5]
+geos, trjs = [], []
+for i in range(191, 200):
+    len_trj = subj[i, 2]
+    x, trj = gs.linspace(0., 1., len_trj), points[ids[i]:ids[i] + len_trj]
     # set warm start
     reg.intercept_ = trj[0]
-    reg.coef_ = KenMetric.log(trj[-1], trj[0])
+    reg.coef_ = S2_metric.log(point=trj[-1], base_point=trj[0])
     # compute best fitting geodesic
     reg.fit(x, trj, compute_training_score=True)
     print('R^2:', reg.training_score_)
     p, u = reg.intercept_, reg.coef_
     geos.append(gs.reshape([p, u], sas.shape))
+    trjs.append(trj)
+geos = gs.array(geos)
+geo_list = []
+t = gs.linspace(0., 1., 25)
+for geo in geos:
+    geo_list.append(S2_metric.geodesic(geo[0], initial_tangent_vec=geo[1])(t))
+visSphere([geo_list] + [trjs], ['r'] + ['b'], size=15)
+#visEarth([geo_list] + [trjs])
 
-    geodesic = KenMetric.geodesic(initial_point=p, initial_tangent_vec=u)(t)
-    geodesics.append(geodesic)
-
-# mean geodesic
-mean_gs = FrechetMean(sas)
+# group mean
+print('Computing mean of geodesics')
+initial = initial_mean(geos, sas.metric)
+mean_gs = FrechetMean(sas, init_point=initial)
 mean_gs.fit(geos)
-mean = gs.reshape(mean_gs.estimate_, (2, *Ken.shape))
-mean_geo = KenMetric.geodesic(initial_point=mean[0], initial_tangent_vec=mean[1])(t)
-geodesics.append(mean_geo)
-geodesics = gs.array(geodesics).reshape(n_subjects+1, Nt*k_landmarks, dim)
-visKen([geodesics[0:n_trj-1]]+[geodesics[-1]], ['b']+['k'])  # TODO
+mean = mean_gs.estimate_
 
-# TPCA
-tpca = TangentPCA(metric=KenMetric, n_components=2)
-tpca = tpca.fit(geos, base_point=mean)
-tangent_projected_data = tpca.transform(geos)
-geo_0 = KenMetric.geodesic(initial_point=mean, initial_tangent_vec=tpca.components_[0])(t)
-geo_1 = KenMetric.geodesic(initial_point=mean, initial_tangent_vec=tpca.components_[1])(t)
-visKenPCA([geo_0]+[geo_1], tpca.explained_variance_ratio_, samples, mean)
-# Learning
+data, meanvalue, geom = [], [], []
+t = gs.linspace(0., 1., 20)
+meanvalue.append(S2_metric.geodesic(mean[0], initial_tangent_vec=mean[1])(t))
+geom.append(S2_metric.geodesic(mean[0], initial_tangent_vec=mean[1])(t))
+for geo in geos:
+    data.append(S2_metric.geodesic(geo[0], initial_tangent_vec=geo[1])(t))
+visSphere([data] + [meanvalue] + [geom], ['r'] + ['k'] + ['b'], size=10)
+
+# Tangent PCA
+tpca = TangentPCA(metric=sas, n_components=2)
+geos_proj = tpca.fit_transform(geos, base_point=mean_gs.estimate_)
+
+# visualize tangent PCA
+visTPCA(tpca.explained_variance_, geos_proj)
